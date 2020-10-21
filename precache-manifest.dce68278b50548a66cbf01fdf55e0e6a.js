287b11cff3fc76565d57b658fa1248ccf0bc9d87
Begad <64358501+begadsalah@users.noreply.github.com>
2020-10-21 13:00:34 +0200

    Updates

---
+++ precache-manifest.dce68278b50548a66cbf01fdf55e0e6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f833200ca08f67bc362fbda72d45e2da",
+    "url": "/itpeaks-website/index.html"
+  },
+  {
+    "revision": "e8c7fb442bcc7eee17e4",
+    "url": "/itpeaks-website/static/css/2.d72ee50e.chunk.css"
+  },
+  {
+    "revision": "53dc7742227b01469e62",
+    "url": "/itpeaks-website/static/css/main.4d18150a.chunk.css"
+  },
+  {
+    "revision": "e8c7fb442bcc7eee17e4",
+    "url": "/itpeaks-website/static/js/2.682e02bd.chunk.js"
+  },
+  {
+    "revision": "a4c32848a4950323e38569f09ef75ffe",
+    "url": "/itpeaks-website/static/js/2.682e02bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53dc7742227b01469e62",
+    "url": "/itpeaks-website/static/js/main.d42c7fd3.chunk.js"
+  },
+  {
+    "revision": "1ee826df2101194fc76e",
+    "url": "/itpeaks-website/static/js/runtime-main.13f33ca2.js"
+  }
+]);
